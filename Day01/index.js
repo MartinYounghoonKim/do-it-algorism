b7df1df3a7cb71d0705e78abeaf44f54fa8cd882
martinyounghoonkim <hoons0131@gmail.com>
2018-08-14 23:25:51 +0900

    Add day01's example

--- Day01/index.js
+++ Day01/index.js
@@ -1,17 +1,11 @@
 function solution(s) {
-    generateLength(s);
+    const stringLength = s.length;
     var answer = '';
+    if ( stringLength % 2 === 0 ) {
+        answer = s.slice((stringLength/2)-1, (stringLength/2) + 1);
+    } else {
+        answer = s[Math.floor(stringLength/2)];
+    }
+
     return answer;
 }
-
-function generateLength (s) {
-    const stringLength = s.length;
-    console.log(stringLength/2);
-    if ( stringLength % 2 === 0 ) {
-
-    } else {
-
-    }
-}
-
-solution('teste');
