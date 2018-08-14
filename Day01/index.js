011b9f245e03cbd212facf7176ed5e34ab37f18c
martinyounghoonkim <hoons0131@gmail.com>
2018-08-14 10:32:01 +0900

    Update day01 example

---
+++ Day01/index.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    generateLength(s);
+    var answer = '';
+    return answer;
+}
+
+function generateLength (s) {
+    const stringLength = s.length;
+    console.log(stringLength/2);
+    if ( stringLength % 2 === 0 ) {
+
+    } else {
+
+    }
+}
+
+solution('teste');
