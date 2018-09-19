4dc8457652a65732d4bfc247957bc400518145c5
martin <martin@within.co.kr>
2018-09-19 23:59:32 +0900

    Add Day11's algorism answer

--- Day11/index.js
+++ Day11/index.js
@@ -1,3 +1,11 @@
 function solution(a, b) {
-
+    let result = [];
+    for (let i = 1; i <= b; i++) {
+        result.push(a * i);
+    }
+    return result;
 }
+
+solution(2, 5);
+solution(4, 3);
+solution(-4, 2);
