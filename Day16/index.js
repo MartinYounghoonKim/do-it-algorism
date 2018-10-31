a72de34d3e83893f5fd0b4117812496f35ea9693
martin <martin@tmon.co.kr>
2018-10-31 09:44:48 +0900

    Add Day16's example

--- Day16/index.js
+++ Day16/index.js
@@ -1,40 +1,44 @@
-let top = -1;
 function move(from, to){
   console.log(`${from}에서 ${to} 로 이동한다.`);
 }
 
-function hanoi (n, from, by, to) {
-  const arr = [];
-  let temp = 1;
-  while (temp > 0) {
-    while (n >1) {
-      arr.push(to);
-      arr.push(to);
-      arr.push(by);
-      arr.push(from);
-      arr.push(n);
-      n--;
-      arr.push(to);
-      to = by;
-      by = arr.pop();
-    }
-    move(from, to);
-    if (arr.length -1 > 0) {
-      n = arr.pop();
-      from = arr.pop();
-      by = arr.pop();
-      to = arr.pop();
-      move(from, to);
-      n --;
-      arr.push(from);
-      from = by;
-      by = arr.pop();
-    } else {
-      temp = 0;
+function solution (n) {
+  const result = [];
+  function hanoi (n, from, to) {
+    const arr = [];
+    let temp = 1;
+    let by = 6-from-to;
+    while (temp > 0) {
+      while (n >1) {
+        arr.push(to);
+        arr.push(by);
+        arr.push(from);
+        arr.push(n);
+        n--;
+        arr.push(to);
+        to = by;
+        by = arr.pop();
+      }
+      result.push([from, to]);
+      // move(from, to)
+      if (arr.length -1 > 0) {
+        n = arr.pop();
+        from = arr.pop();
+        by = arr.pop();
+        to = arr.pop();
+        result.push([from, to]);
+        // move(from, to)
+        n --;
+        arr.push(from);
+        from = by;
+        by = arr.pop();
+      } else {
+        temp = 0;
+      }
     }
   }
+  hanoi (n, 1, 3);
+  return result
 }
-
-hanoi (3, 1, 2, 3);
 
 
