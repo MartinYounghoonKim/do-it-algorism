f203e27ad51782e86d3739341de4b101eae7d8a8
martinyounghoonkim <hoons0131@gmail.com>
2019-01-20 12:43:39 +0900

    Add Day19 answer

--- Day19/index.js
+++ Day19/index.js
@@ -1,4 +1,15 @@
 function solution(n, s) {
   var answer = [];
+  if (n > s) {
+    return [-1];
+  }
+
+  for (let i = 0; i < n; i++) {
+    const number = Math.floor(s/(n-i));
+    answer.push(number);
+    s = s - number;
+  }
+
   return answer;
 }
+solution(35, 179);
