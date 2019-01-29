03b1a559c53af359c87622acb8b21e77f58a25aa
martinyounghoonkim <hoons0131@gmail.com>
2019-01-29 23:40:57 +0900

    Add Day19 answer with test code

--- Day19/index.js
+++ Day19/index.js
@@ -9,7 +9,24 @@ function solution(n, s) {
     answer.push(number);
     s = s - number;
   }
-
+  console.log(answer);
   return answer;
 }
-solution(35, 179);
+solution(5, 124);
+
+function solution1(n, s) {
+  var answer = [];
+  if (n > s) {
+    return [-1];
+  }
+
+  for (let i = 0; i < n; i++) {
+    const number = Math.round(s/(n-i));
+    answer.push(number);
+    s = s - number;
+  }
+  console.log(answer);
+  return answer;
+}
+solution1(5, 124);
+
