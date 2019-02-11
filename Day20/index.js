f533bcac889e2337c391e114387986a1da00d26e
martinyounghoonkim <hoons0131@gmail.com>
2019-02-11 23:55:16 +0900

    Update Day20 answer with test code

--- Day20/index.js
+++ Day20/index.js
@@ -1,4 +1,21 @@
 function solution(clothes) {
-  var answer = 0;
-  return answer;
+  const hashmap = {};
+  let answer = 1;
+  while (clothes.length > 0) {
+    const option = clothes.shift();
+    const key = option[1];
+
+    if (hashmap.hasOwnProperty(key)) {
+      hashmap[key] += 1;
+    } else {
+      hashmap[key] = 1;
+    }
+  }
+  for (const key in hashmap) {
+    answer *= (hashmap[key] + 1);
+  }
+  return answer - 1;
 }
+
+console.log(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]));
+console.log(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]));
