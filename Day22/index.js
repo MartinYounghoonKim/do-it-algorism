2a1bfecdcf2f2fba1e0fdde29fc6558cc5497957
martinyounghoonkim <hoons0131@gmail.com>
2019-04-08 21:54:10 +0900

    Update Day22 answer

--- Day22/index.js
+++ Day22/index.js
@@ -1,21 +1,24 @@
 function solution(participant, completion) {
   const hashmap = {};
   let result = "";
-  for (let i = 0; i < participant.length; i++) {
-    const key = participant[i];
-    if (hashmap[key] > 1) {
-      hashmap[key] += 1;
-    } else {
+  for (let i = 0; i < completion.length; i++) {
+    const key = completion[i];
+    if (!hashmap[key]) {
       hashmap[key] = 1;
+    } else {
+      hashmap[key] += 1;
     }
   }
-  for (let i = 0; i < completion.length; i++) {
-    if (hashmap[completion[i]]) {
-      hashmap[completion[i]] -= 1;
+  for (let i = 0; i < participant.length; i++) {
+    const key = participant[i];
+    if (!hashmap[key]) {
+      result = participant[i];
+      break;
+    } else {
+      hashmap[key] -= 1;
     }
-    result = completion[i];
   }
   return result;
 }
 
-solution(["leo", "kiki", "eden"], ["eden", "kiki"]);
+solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]);
